b817226d870c9b36f6ff95bb238c5055c6ad4708
samson.ssali <samson.ssali@sparkz.systemz>
2026-05-10 18:06:56 -0600

    Fix QrCodeHelper: use IBufferByteAccess COM interface for WinUI 3 raw buffer access

--- platforms/windows/QrCodeHelper.cs
+++ platforms/windows/QrCodeHelper.cs
@@ -1,21 +1,22 @@
 using Microsoft.UI.Xaml.Media.Imaging;
 using QRCoder;
 using System.Runtime.InteropServices;
+using Windows.Storage.Streams;
 
 namespace Seyfr
 {
     /// <summary>
     /// Generates a WinUI 3 WriteableBitmap QR code from a text string using QRCoder.
-    /// Uses IMemoryBufferByteAccess to write raw pixels directly to the buffer.
+    /// Uses IBufferByteAccess COM interface to write raw pixels directly to the buffer.
     /// </summary>
     public static class QrCodeHelper
     {
         [ComImport]
-        [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
+        [Guid("905a0fef-bc53-11df-8c49-001e4fc686da")]
         [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        unsafe interface IMemoryBufferByteAccess
+        unsafe interface IBufferByteAccess
         {
-            void GetBuffer(out byte* buffer, out uint capacity);
+            void Buffer(out byte* value);
         }
 
         public static unsafe WriteableBitmap Generate(string text, int pixelsPerModule = 8)
@@ -26,28 +27,25 @@ namespace Seyfr
             int size = moduleCount * pixelsPerModule;
 
             var writeableBitmap = new WriteableBitmap(size, size);
+            IBuffer buffer = writeableBitmap.PixelBuffer;
 
-            using (var reference = writeableBitmap.PixelBuffer.CreateReference())
+            byte* data;
+            ((IBufferByteAccess)buffer).Buffer(out data);
+
+            for (int y = 0; y < size; y++)
             {
-                byte* data;
-                uint capacity;
-                ((IMemoryBufferByteAccess)reference).GetBuffer(out data, out capacity);
-
-                for (int y = 0; y < size; y++)
+                for (int x = 0; x < size; x++)
                 {
-                    for (int x = 0; x < size; x++)
-                    {
-                        int moduleX = x / pixelsPerModule;
-                        int moduleY = y / pixelsPerModule;
-                        bool isBlack = qrCodeData.ModuleMatrix[moduleY][moduleX];
+                    int moduleX = x / pixelsPerModule;
+                    int moduleY = y / pixelsPerModule;
+                    bool isBlack = qrCodeData.ModuleMatrix[moduleY][moduleX];
 
-                        int index = (y * size + x) * 4;
-                        byte color = isBlack ? (byte)0 : (byte)255;
-                        data[index] = color;     // B
-                        data[index + 1] = color; // G
-                        data[index + 2] = color; // R
-                        data[index + 3] = 255;   // A
-                    }
+                    int index = (y * size + x) * 4;
+                    byte color = isBlack ? (byte)0 : (byte)255;
+                    data[index] = color;     // B
+                    data[index + 1] = color; // G
+                    data[index + 2] = color; // R
+                    data[index + 3] = 255;   // A
                 }
             }
 
